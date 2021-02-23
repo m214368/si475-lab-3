84b9eb017d52c066c950aa4a8a8be6d133f2b2ea
m214368 <73185501+m214368@users.noreply.github.com>
2021-02-23 14:01:12 -0500

    more files added

--- demo.py
+++ demo.py
@@ -1,1 +1,12 @@
-#stuff goes here
+import rospy
+from turtleAPI import robot
+
+try:
+  print("creating robot")
+  r= robot()
+  r.drive(angSpeed=.4,linSpeed=.25)
+  while not rospy.is_shutdown():
+    pass
+except Exception as e:
+  print(e)
+  rospy.loginto("node now terminated")
