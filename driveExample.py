c1fef0b6b0411851504af6e7391518437fac3820
Graham Daker <m211332@usna.edu>
2021-02-23 14:49:40 -0500

    should drive one meter

--- driveExample.py
+++ driveExample.py
@@ -4,10 +4,19 @@ from turtleAPI import robot
 try:
     print("creating robot")
     r= robot()
-    r.drive(angSpeed=.4,linSpeed=.25)
-    while not rospy.is_shutdown():
-        print(r.range())
+    r.drive(linSpeed=.25)
+
+    # get initial position
+    init_pos = r.getPositionTup()[0]
+    goal_pos = init_pos + 1
+
+    # checking to see how far it is
+    while r.getPositionTup()[0] < goal_pos:
         pass
+
+    # stop
+    r.drive(linSpeed=0)
+    
 except Exception as e:
   print(e)
   rospy.loginto("node now terminated")
