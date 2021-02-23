4e1f6fcccabe80d5bbaba084e9539d6b7f2a2272
Graham Daker <m211332@usna.edu>
2021-02-23 14:23:30 -0500

    print range

--- driveExample.py
+++ driveExample.py
@@ -2,11 +2,12 @@ import rospy
 from turtleAPI import robot
 
 try:
-  print("creating robot")
-  r= robot()
-  r.drive(angSpeed=.4,linSpeed=.25)
-  while not rospy.is_shutdown():
-    pass
+    print("creating robot")
+    r= robot()
+    r.drive(angSpeed=.4,linSpeed=.25)
+    while not rospy.is_shutdown():
+        print(r.range())
+        pass
 except Exception as e:
   print(e)
   rospy.loginto("node now terminated")
