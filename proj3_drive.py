6c5bf3ce0247c1cf84b093436f75c265411d4576
Graham Daker <m211332@usna.edu>
2021-02-28 15:46:36 -0500

    added stuff back

--- proj3_drive.py
+++ proj3_drive.py
@@ -38,9 +38,8 @@ def pid_speed(kp, ki, kd, error, old_error, error_list):
         error_sum += i
 
     # kp portion + ki portion
-    to_return = kp * error
-    #+ ki * error_sumS
-    #to_return += kd * error - old_error
+    to_return = kp * error + ki * error_sumS
+    to_return += kd * error - old_error
 
     return to_return
 
