9b0794c0b2a50d558a0ad5eb13a0b268078d530e
m214368 <73185501+m214368@users.noreply.github.com>
2021-02-28 21:06:25 -0500

    bumpdemo ready to troubleshoot

--- bumpDemo.py
+++ bumpDemo.py
@@ -2,28 +2,35 @@
 
 import rospy
 from turtleAPI import robot
+from time import sleep
 
 
+gas=4
+wheel=1
 
-
-
+rate = rospy.Rate(10)
 
 try:
   #print("creating robot")
   r= robot()
 
   #drive forward
-  r.drive(angSpeed=0,linSpeed=.75)
+  r.drive(angSpeed=0,linSpeed=gas)
 
   while not rospy.is_shutdown():
   	#check if bump has been detected
     print r.getBumpStatus()
     if (r.getBumpStatus().state == 1) #bump detected
+    	r.drive(angSpeed=0,linSpeed=-gas)
+    	sleep(.5)
     	if (r.getBumpStatus().bumper == 2): #right side bump
-
-    	else if (r.getBumpStatus().bumper == 1): #middle bump
-
+    		r.drive(angSpeed=-wheel,linSpeed=gas)
+    	else if (r.getBumpStatus().bumper == 0): #left side
+    		r.drive(angSpeed=wheel,linSpeed=gas)
     	else: #left pump
+    		r.drive(angSpeed=wheel,linSpeed=0)
+		sleep(.5)
+	rate.sleep()
 except Exception as e:
   print(e)
   rospy.loginto("node now terminated")
